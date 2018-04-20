4a2bba2f8855a1c0934cfbc9de6b4511989ffdd0
HYAN FERNANDES MANDAIN <ALU201314277@uniritter.local>
2018-04-20 20:02:57 -0300

    :tada:

---
+++ src/main/java/uniritter/edu/br/jmonitor/JmonitorApplication.java
@@ -0,0 +1,12 @@
+package uniritter.edu.br.jmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmonitorApplication.class, args);
+	}
+}
