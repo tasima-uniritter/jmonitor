0661a6024b9b3a34cc518cae3209cd479b118e69
LUIS FERNANDO GOMES SANTOS <ALU201731856@uniritter.local>
2018-06-09 11:29:54 -0300

    Uncomment routes

--- src/main/java/br/edu/uniritter/monitors/route/MonitorRoute.java
+++ src/main/java/br/edu/uniritter/monitors/route/MonitorRoute.java
@@ -6,7 +6,7 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class MonitorRoute extends RouteBuilder {
 
     @Override
