53f09301de0284db22867df38e135b73ce76daa7
Luis Fernando Gomes <dev@luiscoms.com.br>
2018-06-15 15:30:56 -0300

    Remove unused

--- src/test/java/br/edu/uniritter/monitors/ApplicationTests.java
+++ src/test/java/br/edu/uniritter/monitors/ApplicationTests.java
@@ -3,7 +3,6 @@ package br.edu.uniritter.monitors;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
